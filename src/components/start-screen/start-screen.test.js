d4922adbcb751efe545e2dd40da306ef5171ce3e
DmitriyS87 <schukindmitriy@gmail.com>
2019-05-09 04:02:40 +0800

    makes StartScreen snapshot test

---
+++ src/components/start-screen/start-screen.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import StartScreen from './start-screen/start-screen.jsx';
+
+it(`StartScreen correctly rendered`, () => {
+  const tree = renderer
+  .create(<StartScreen
+    time={0}
+    errorCount={0}
+    onclick={jest.fn()}
+  />)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
