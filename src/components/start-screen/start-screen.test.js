cb845c4fb2fd2081e5bbfb3aca5bfc3d69ba9271
DmitriyS87 <schukindmitriy@gmail.com>
2019-05-09 04:22:01 +0800

    fixes test script value and path of components in tests files

--- src/components/start-screen/start-screen.test.js
+++ src/components/start-screen/start-screen.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import StartScreen from './start-screen/start-screen.jsx';
+import StartScreen from '../start-screen/start-screen.jsx';
 
 it(`StartScreen correctly rendered`, () => {
   const tree = renderer
