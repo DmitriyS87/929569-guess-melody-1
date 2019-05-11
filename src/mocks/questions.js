98d0a2fa891fc8c55d3adaa2c51c03835896f929
DmitriyS87 <schukindmitriy@gmail.com>
2019-05-11 18:43:08 +0800

    makes mocks settings and questions

---
+++ src/mocks/questions.js
@@ -0,0 +1,42 @@
+export default [{
+  type: `artist`,
+  artist: `Basta`,
+  answers: [
+    {
+      artist: `Philipp Kirkorov`,
+      picture: ``
+    },
+    {
+      artist: `Slava KPSS`,
+      picture: ``
+    },
+    {
+      artist: `Basta`,
+      picture: ``
+    }
+  ]
+},
+{
+  type: `genre`,
+  genre: `rap`,
+  answers: [
+    {
+      genre: `rap`,
+      url: ``
+    },
+    {
+      genre: `rock`,
+      url: ``
+    },
+    {
+      genre: `jass`,
+      url: ``
+    },
+    {
+      genre: `pop`,
+      url: ``
+    },
+
+  ]
+}
+];
