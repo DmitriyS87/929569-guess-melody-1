9983a66765d472246f906a3d9831068eb64c0de6
DmitriyS87 <schukindmitriy@gmail.com>
2019-05-19 14:14:28 +0800

    modifies mock questions

--- src/mocks/questions.js
+++ src/mocks/questions.js
@@ -1,42 +1,125 @@
-export default [{
-  type: `artist`,
-  artist: `Basta`,
-  answers: [
-    {
-      artist: `Philipp Kirkorov`,
-      picture: ``
-    },
-    {
-      artist: `Slava KPSS`,
-      picture: ``
-    },
-    {
-      artist: `Basta`,
-      picture: ``
-    }
-  ]
-},
-{
-  type: `genre`,
-  genre: `rap`,
-  answers: [
-    {
-      genre: `rap`,
-      url: ``
-    },
-    {
-      genre: `rock`,
-      url: ``
-    },
-    {
-      genre: `jass`,
-      url: ``
-    },
-    {
-      genre: `pop`,
-      url: ``
-    },
+export default [
+  {
+    type: `artist`,
+    artist: `Basta`,
+    answers: [
+      {
+        artist: `Philipp Kirkorov`,
+        picture: ``
+      },
+      {
+        artist: `Slava KPSS`,
+        picture: ``
+      },
+      {
+        artist: `Basta`,
+        picture: ``
+      }
+    ]
+  },
+  {
+    type: `artist`,
+    artist: `Morcheeba`,
+    answers: [
+      {
+        artist: `Morcheeba`,
+        picture: ``
+      },
+      {
+        artist: `Celine Dion`,
+        picture: ``
+      },
+      {
+        artist: `Olga Buzova`,
+        picture: ``
+      }
+    ]
+  },
+  {
+    type: `artist`,
+    artist: `25 17`,
+    answers: [
+      {
+        artist: `Slava KPSS`,
+        picture: ``
+      },
+      {
+        artist: `Krec`,
+        picture: ``
+      },
+      {
+        artist: `25 17`,
+        picture: ``
+      }
+    ]
+  },
+  {
+    type: `genre`,
+    genre: `rap`,
+    answers: [
+      {
+        genre: `rap`,
+        url: ``
+      },
+      {
+        genre: `pop`,
+        url: ``
+      },
+      {
+        genre: `hip-hop`,
+        url: ``
+      },
+      {
+        genre: `rap`,
+        url: ``
+      },
 
-  ]
-}
+    ]
+  },
+  {
+    type: `genre`,
+    genre: `pop`,
+    answers: [
+      {
+        genre: `blues`,
+        url: ``
+      },
+      {
+        genre: `rock`,
+        url: ``
+      },
+      {
+        genre: `jass`,
+        url: ``
+      },
+      {
+        genre: `pop`,
+        url: ``
+      },
+
+    ]
+  },
+  {
+    type: `genre`,
+    genre: `jass`,
+    answers: [
+      {
+        genre: `blues`,
+        url: ``
+      },
+      {
+        genre: `jass`,
+        url: ``
+      },
+      {
+        genre: `jass`,
+        url: ``
+      },
+      {
+        genre: `blues`,
+        url: ``
+      },
+
+    ]
+  }
 ];
