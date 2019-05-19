2d9cb93689a880a2549535f1e08d5ae2621382cb
DmitriyS87 <schukindmitriy@gmail.com>
2019-05-19 14:10:45 +0800

    makes ArtistQuestionScreen with snapshot

--- src/components/artist-question-screen/artist-question-screen.jsx
+++ src/components/artist-question-screen/artist-question-screen.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 class ArtistQuestionScreen extends React.PureComponent {
   render() {
-    const {question, handlerAnswerClick, handlerPlayClick, handlerSubmit} = this.props;
+    const {question, onAnswer, handlerPlayClick, handlerSubmit} = this.props;
     return (
       <section className="game game--artist">
         <header className="game__header">
@@ -13,7 +13,8 @@ class ArtistQuestionScreen extends React.PureComponent {
           </a>
 
           <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
-            <circle className="timer__line" cx="390" cy="390" r="370" style="filter: url(#blur); transform: rotate(-90deg) scaleY(-1); transform-origin: center" />
+            <circle className="timer__line" cx="390" cy="390" r="370"
+              style={{filter: `url(#blur)`, transform: `rotate(-90deg) scaleY(-1)`, transformOrigin: `center`}} />
           </svg>
 
           <div className="timer__value" xmlns="http://www.w3.org/1999/xhtml">
@@ -28,19 +29,17 @@ class ArtistQuestionScreen extends React.PureComponent {
             <div className="wrong"></div>
           </div>
         </header>
-
         <section className="game__screen">
           <h2 className="game__title">Кто исполняет эту песню?</h2>
           <div className="game__track">
             <button className="track__button track__button--play" type="button" onClick={handlerPlayClick}></button>
             <audio></audio>
           </div>
-
           <form className="game__artist" onSubmit={handlerSubmit}>
             {question.answers.map((answer, index) => {
               return (
                 <div key={index} className="artist">
-                  <input className="artist__input visually-hidden" type="radio" name="answer" value={answer.artist} id={`answer-` + index} onClick={handlerAnswerClick} />
+                  <input className="artist__input visually-hidden" type="radio" name="answer" value={answer.artist} id={`answer-` + index} onClick={(evt) => onAnswer(evt.target.value)} />
                   <label className="artist__name" htmlFor={`answer-` + index}>
                     <img className="artist__picture" src={answer.picture} alt={answer.artist} />
                     {answer.artist}
@@ -50,13 +49,15 @@ class ArtistQuestionScreen extends React.PureComponent {
           </form>
         </section>
       </section>
+
+
     );
   }
 }
 
 ArtistQuestionScreen.propTypes = {
   question: PropTypes.object.isRequired,
-  handlerAnswerClick: PropTypes.func,
+  onAnswer: PropTypes.func,
   handlerPlayClick: PropTypes.func,
   handlerSubmit: PropTypes.func
 };
